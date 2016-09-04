0591eb8dbad64ec42663427d877c3db8e035736e
Adam F <adam.fonfarksi@student.uts.edu.au>
2016-09-05 03:11:38 +1000

    + Maze positioning changed - Also added some layout changes + Added cube walls which finally line up with the path

--- GamesProgAssignment4/GamesProgAssignment4/Source/GameObjects/GroundPrimitive.cs
+++ GamesProgAssignment4/GamesProgAssignment4/Source/GameObjects/GroundPrimitive.cs
@@ -14,26 +14,22 @@ namespace GamesProgAssignment4
         int tileSize;
         int numTilesAcross;
         int totalSize;
-        Vector3 centredPosition;
 
-        public GroundPrimitive(Game game, ObjectManager objectManager, Vector3 position, BasicCamera camera, GraphicsDevice graphicsDevice, Texture2D texture, int tileSize, int numTilesAcross, bool isGrid) :
+        public GroundPrimitive(Game game, ObjectManager objectManager, Vector3 position, BasicCamera camera, GraphicsDevice graphicsDevice, Texture2D texture, int tileSize, int numTilesAcross) :
             base(game, objectManager, position, camera, graphicsDevice, texture)
         {
             this.tileSize = tileSize;
             this.numTilesAcross = numTilesAcross;
             totalSize = tileSize * numTilesAcross;
             hasLighting = false;
-            //centredPosition = new Vector3(position.X / 2, 0, position.Z / 2);
-            centredPosition = new Vector3(-tileSize / 2, 0, -tileSize / 2);
 
-            translation = Matrix.CreateTranslation(centredPosition);
-            //translation = Matrix.CreateTranslation(position);
-
-            if (isGrid) {
+            if (numTilesAcross > 1) {
                 constructGridPlane();
             } else {
                 constructSingularPlane();
             }
+
+            translation = Matrix.CreateTranslation(position);
         }
 
         public override Matrix getWorld()
@@ -78,12 +74,12 @@ namespace GamesProgAssignment4
             vertexData = new VertexPositionNormalTexture[4];
             indexData = new int[6];
 
-            Vector3 topLeft = new Vector3(position.X, position.Y, position.Z + tileSize);
-            Vector3 topRight = new Vector3(position.X + tileSize, position.Y, position.Z + tileSize);
-            Vector3 bottomLeft = new Vector3(position.X, position.Y, position.Z);
-            Vector3 bottomRight = new Vector3(position.X + tileSize, position.Y, position.Z);
+            Vector3 topLeft = new Vector3(position.X, position.Y, position.Z);
+            Vector3 topRight = new Vector3(position.X + tileSize, position.Y, position.Z);
+            Vector3 bottomLeft = new Vector3(position.X, position.Y, position.Z + tileSize);
+            Vector3 bottomRight = new Vector3(position.X + tileSize, position.Y, position.Z + tileSize);
 
-
+            
             vertexData[0] = new VertexPositionNormalTexture(topLeft, Vector3.Up, new Vector2(0, 0));
             vertexData[1] = new VertexPositionNormalTexture(topRight, Vector3.Up, new Vector2(1, 0));
             vertexData[2] = new VertexPositionNormalTexture(bottomLeft, Vector3.Up, new Vector2(0, 1));
@@ -95,10 +91,7 @@ namespace GamesProgAssignment4
             indexData[3] = 2;
             indexData[4] = 1;
             indexData[5] = 3;
-
-
-
-
+            
         }
     }
 }

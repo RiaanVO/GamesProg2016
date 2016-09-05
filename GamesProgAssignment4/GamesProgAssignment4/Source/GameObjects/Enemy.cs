f0bb70923a7df54deb9db16b6626a4b237a77294
Riaan Van Onselen <Riaanvo@yahoo.com.sg>
2016-09-05 19:51:52 +1000

    Added enemy collision, and sound effects. Need to work on the 3d part of heart beat. and watch your ears :)

--- GamesProgAssignment4/GamesProgAssignment4/Source/GameObjects/Enemy.cs
+++ GamesProgAssignment4/GamesProgAssignment4/Source/GameObjects/Enemy.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 
 namespace GamesProgAssignment4
 {
@@ -20,25 +21,15 @@ namespace GamesProgAssignment4
         float orientationDifference = 0.001f;
 
         Vector3 velocity;
-        float maxVelocity = 20f;
+        float maxVelocity = 10f;
         float acceleration = 0.5f;
 
         float stoppingDistFromTarget = 1;
 
-        //old fields:
-        /*Player player;
+        SphereCollider collider;
+        AudioEmitterComponent audioEmitter;
 
-        Vector3 movementDirection;
-        Vector3 velocity, acceleration;
-        float localRotation;
-
-        float maxVelocity = 5f; //temporary test value
-        float accelerationRate = 1f;
-        float drag = 0.1f;
-        float slowestSpeed = 0.5f;
-        */
-
-        //BoxCollider box;
+        bool playerCaught = false;
 
         public Enemy(Game game, ObjectManager objectManager, Vector3 startPosition, Model model, BasicCamera camera, Player player) : base(game, objectManager, startPosition, model, camera)
         {
@@ -49,30 +40,30 @@ namespace GamesProgAssignment4
             orientation = (float)Math.Atan2(lookDirection.X, lookDirection.Z);
             targetPosition = player.position;
             velocity = Vector3.Zero;
+            hasLighting = false;
 
-            /*this.player = player; //so it can get the player's position for pursuing
-            position = startPosition;
-            hasLighting = true;
-            scale = Matrix.CreateScale(5f);
-            *///vary depending on size of ghost model (haven't been able to get one to work yet :/)
-        }
+            collider = new SphereCollider(game, this, objectTag.enemy, true, true, 2);
+            audioEmitter = new AudioEmitterComponent(game, this);
+            audioEmitter.createSoundEffectInstance("heartbeat", game.Content.Load<SoundEffect>(@"Sounds/heartbeat"), true, true, true);
+            audioEmitter.addSoundEffect("Scream", game.Content.Load<SoundEffect>(@"Sounds/scaryscream"));
 
-        public override void Initialize()
-        {
-            /*velocity = Vector3.Zero;
-            movementDirection = Vector3.Zero;
-            acceleration = Vector3.Zero;
-            localRotation = 0f;
-
-            translation = Matrix.CreateTranslation(position);
-            */
-            base.Initialize();
         }
 
         public override void Update(GameTime gameTime)
         {
             //if (player.position != position) handleMovement(gameTime);
             handleMovement(gameTime);
+
+            if (collider.collidingWith.Count != 0) {
+                foreach(Collider col in collider.collidingWith)
+                {
+                    if(col.tag == objectTag.player && !playerCaught)
+                    {
+                        audioEmitter.playSoundEffect("Scream");
+                        playerCaught = true;
+                    }
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -109,38 +100,6 @@ namespace GamesProgAssignment4
 
             translation = Matrix.CreateTranslation(position);
             rotation = Matrix.CreateRotationY(orientation);
-
-            /*Vector3 difference = player.position - position;
-            difference.Normalize();
-
-            //handles XYZ movement (translate)
-
-            //acceleration = Vector3.Zero;
-            acceleration = Vector3.Multiply(difference, accelerationRate);
-            acceleration.Y = 0;
-
-            velocity -= Vector3.Multiply(velocity, drag);
-
-            if ((velocity).Length() < slowestSpeed)
-            {
-                velocity = Vector3.Zero;
-            }
-
-            velocity += acceleration * deltaTime;
-
-            if ((velocity).Length() > maxVelocity)
-            {
-                velocity.Normalize();
-                velocity = Vector3.Multiply(velocity, maxVelocity);
-            }
-
-            position += velocity * deltaTime;
-            translation = Matrix.CreateTranslation(position);
-
-            //handle rotation (on Y axis) (no other axes needed at this stage)
-
-            localRotation = Vector3ToRadian(difference);
-            rotation = Matrix.CreateRotationY(localRotation); */
         }
 
         private float Vector3ToRadian (Vector3 direction)

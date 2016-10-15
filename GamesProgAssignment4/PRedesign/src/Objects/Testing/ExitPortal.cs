8f7e7e2e60b0c4ff59ee3d046c889f691f2ffc3e
HarrisonSmith-UTS <harrison.pj.smith@gmail.com>
2016-10-15 20:06:12 +1100

    Added Exit portal

--- GamesProgAssignment4/PRedesign/src/Objects/Testing/ExitPortal.cs
+++ GamesProgAssignment4/PRedesign/src/Objects/Testing/ExitPortal.cs
@@ -21,16 +21,10 @@ namespace PRedesign.src.Objects.Testing
         AudioEmitterComponent audioEmitter;
 
         // animation variables
-        private bool opening = false;
-        private bool isSplit;
         private float deltaTime;
-        private float rotationalSpeed = 1f; //speed of rotation
-        private float activateDistance = 35f; //distance between player and object to activate
-        private float outerMaxDistance = 25f; //how far the outer parts can move
-        private float currentDistance; //how far the outers are currently
-        private float outerMovementSpeed = 2f; //how quickly the outer parts move
 
         // hover animation variables
+        private float hoverLimit = 0.8f;
         private float hoverHeight;
         private float originalYPosition;
         private float hoverSpeed = 0.8f;
@@ -52,24 +46,16 @@ namespace PRedesign.src.Objects.Testing
             audioEmitter = new AudioEmitterComponent(this);
             //audioEmitter.addSoundEffect("pickup", game.Content.Load<SoundEffect>(@"Sounds/key"));
 
-            //Set up animation variables
-            isSplit = false;
-            //scaleMatrix = Matrix.CreateScale(0.05f);
-            currentDistance = 0f;
             hoverHeight = 0f;
             originalYPosition = startPosition.Y;
-
             hasLighting = true;
         }
 
         public override void Update(GameTime gameTime)
         {
             //Doesn't do anything out of ordinary if locked
-            /*Exit portal should just stay open.
-            if (Vector3.Distance(this.position, player.Position) < activateDistance)
-                opening = true;
-            else
-                opening = false;*/
+            //Exit portal should just stay open.
+            deltaTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000;
 
             List<Collider> currentCollisions = collider.getCollisions();
             if (currentCollisions.Count() > 0)
@@ -87,53 +73,22 @@ namespace PRedesign.src.Objects.Testing
 
             //Animate the unlocking sequence if needed
             //Animate opening if player is close
-            
+            hoverAnimation();
 
             base.Update(gameTime);
         }
 
-        private void rotateKey(GameTime gameTime)
+        private void hoverAnimation()
         {
-            deltaTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000;
-
-            orientation += rotationalSpeed * deltaTime;
+            hoverHeight += hoverSpeed * deltaTime;
             //resets to zero + overlap
-            if (orientation > MathHelper.TwoPi)
+            if (hoverHeight > MathHelper.TwoPi)
             {
-                float overlap = orientation - MathHelper.TwoPi;
-                orientation = 0f + overlap;
+                float overlap = hoverHeight - MathHelper.TwoPi;
+                hoverHeight = 0f + overlap;
             }
-            //Otherwise rotate the outside counterclockwise
-            if (isSplit)
-            {
-                rotationMatrix = Matrix.CreateRotationY(-orientation);
-                model.Bones["top_geo"].Transform = Matrix.CreateRotationY(orientation * 2);
-                model.Bones["bot_geo"].Transform = Matrix.CreateRotationY(orientation * 2);
-            }
-            else
-            {
-                rotationMatrix = Matrix.CreateRotationY(orientation);
-                model.Bones["top_geo"].Transform = Matrix.CreateRotationY(0f);
-                model.Bones["bot_geo"].Transform = Matrix.CreateRotationY(0f);
-            }
-        }
 
-        private void unlockAnimation()
-        {
-            //Moves them outwards or inwards
-            //Needs to accelerate and decelerate as they go past? or just move it out until it hits the limit
-            if (isSplit && currentDistance <= outerMaxDistance)
-            {
-                currentDistance += outerMovementSpeed;
-            }
-            if (!isSplit && currentDistance > 0)
-            {
-                currentDistance -= outerMovementSpeed;
-            }
-            //currentDistance += (isSplit ? outerMovementSpeed : -outerMovementSpeed);
-            model.Bones["top_geo"].Transform *= Matrix.CreateTranslation(0f, currentDistance, 0f);
-            model.Bones["bot_geo"].Transform *= Matrix.CreateTranslation(0f, -currentDistance, 0f);
-
+            position.Y = originalYPosition + (float)(Math.Sin(hoverHeight) * hoverLimit);
         }
 
         public override Matrix GetWorld()
